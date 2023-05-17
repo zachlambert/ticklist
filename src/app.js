59ed3ca517ad52d7cdca807e410ad979516c5bce
Zach Lambert <zachalambert@gmail.com>
2023-05-18 00:28:22 +0100

    Can add items to lists

--- src/app.js
+++ src/app.js
@@ -1,14 +1,37 @@
 
+import { useState } from 'react';
+
 function List({name}) {
+
+  const [items, _] = useState([])
+  const [newItem, setNewItem] =useState("")
+
   return (
     <div className='list'>
       <h2 className='list-title'>{name}</h2>
-      <ul className='list-text'>
-        <li>Item a</li>
-        <li>Item b</li>
+      <ul className='list-content'>
+        {
+          items.map((item, idx) => {
+            return (<li className='list-item list-item-text' key={idx}>{item}</li>);
+          })
+        }
+        <li className="list-item list-item-input">
+          <input type="text" className="list-item" value={newItem}
+            onChange={(event) => {
+              setNewItem(event.target.value);
+            }}
+            onKeyDown={(event) => {
+              if (event.key == 'Enter' && newItem.length > 0) {
+                console.log(`Add new item: ${newItem}`);
+                items.push(newItem);
+                setNewItem("");
+              }
+            }}
+          ></input>
+        </li>
       </ul>
     </div>
-  )
+  );
 }
 
 function App() {
