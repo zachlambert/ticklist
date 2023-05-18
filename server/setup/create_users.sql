4fd494599081587dddf60599d2f94edc51cb9bd3
Zach Lambert <zachalambert@gmail.com>
2023-05-19 00:19:50 +0100

    Created scripts tos setup database tables

---
+++ server/setup/create_users.sql
@@ -0,0 +1,6 @@
+-- TODO: Use proper passwords
+-- TODO: Add reader and writer roles
+CREATE ROLE admin WITH LOGIN PASSWORD 'admin';
+ALTER ROLE admin CREATEDB;
+
+-- TODO: Grant permissions to admin
