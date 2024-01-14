c153a76501f9d9a5113ef3b72b3c88bc4190bc04
Zach Lambert <zachalambert@gmail.com>
2024-01-14 15:23:41 +0000

    Added get_items, get_item to cli

--- ticklist-server/src/bin/cli.rs
+++ ticklist-server/src/bin/cli.rs
@@ -4,11 +4,39 @@ use ticklist::{
 };
 use futures::executor::block_on;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: CliCommand,
+}
+
+#[derive(Subcommand)]
+enum CliCommand {
+    GetItems,
+    GetItem{
+        #[arg(short, long)]
+        slug: String,
+    },
+}
+
+
 fn main() -> Result<(), String> {
     let conn = block_on(get_database_conn())?;
-    let items = block_on(get_items(&conn))?;
-    println!("{:?}", items);
-    let item = block_on(get_item_by_slug(&conn, "attack-on-titan-anime-series"));
-    println!("{:?}", item);
+
+    let cli = Cli::parse();
+    match cli.command {
+        CliCommand::GetItems => {
+            let items = block_on(get_items(&conn))?;
+            println!("{:?}", items);
+        },
+        CliCommand::GetItem{ slug } => {
+            match block_on(get_item_by_slug(&conn, &slug))? {
+                Some(item) => println!("{:?}", item),
+                None => println!("No item with slug '{:}'", slug),
+            }
+        },
+    }
     Ok(())
 }
